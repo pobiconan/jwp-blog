c273658dd7386f297e53704c92078748bcc5fb0d
포비 <pobi@woowahan.com>
2019-07-09 17:35:52 +0900

    add html form submit

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,10 +2,20 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import techcourse.myblog.domain.Article;
 import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+
+    @PostMapping("/articles")
+    public String create(Article article, Model model) {
+        System.out.println("Article : " + article);
+        model.addAttribute("article", article);
+        return "article";
+    }
 }
